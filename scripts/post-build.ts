8f0842d2a2ba116b4809c93a50c5bc6bbdd929aa
Szymon Tondowski <stdeykun+git@gmail.com>
2024-11-17 23:09:26 +0100

    Project init based on maps

---
+++ scripts/post-build.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url'
+import { PATHS_DATA } from '../src/constants';
+
+PATHS_DATA.forEach(({ path, title, social, lang = 'en' }) => {
+  let html = fs.readFileSync('./dist/index.html', 'utf-8');
+
+  html = html.replace('lang="en"', `lang="${lang}"`);
+  html = html.replace('<title>Spes - personalized search</title>', `<title>${title}</title>`);
+  html = html.replace('<!-- SOCIAL -->', social);
+
+  fs.writeFileSync(`./dist/${path}.html`, html);
+});
