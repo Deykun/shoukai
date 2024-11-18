70ee4bb9e9065293544959eb3022c37821cc1661
Szymon Tondowski <stdeykun+git@gmail.com>
2024-11-18 02:55:35 +0100

    ✳ Open search normally and improve Wikipedia matching (#3)

--- src/features/search/pages/SearchPage.tsx
+++ src/features/search/pages/SearchPage.tsx
@@ -1,9 +1,12 @@
 import clsx from "clsx";
 
+import IconSearch from '@/components/Icons/IconSearch';
+
 import useSearchStore from "@/features/search/stores/searchStore";
 
 import SearchBar from "@/features/search/components/SearchBar";
 import SearchResultsSummary from "@/features/search/components/SearchResultsSummary";
+import GoToSearchEngine from "@/features/search/components/GoToSearchEngine";
 import SidebarWikipedia from "@/features/wikipedia/components/SidebarWikipedia";
 
 const SearchPage = () => {
@@ -15,8 +18,12 @@ const SearchPage = () => {
         "mt-[200px]": results.length === 0,
       })}
     >
-      <h1 className="text-[50px] mb-10 font-[600] tracking-wide text-center text-primary-contrast">
-        spes
+      <h1 className={clsx(
+        'flex flex-row justify-center items-center gap-3 mb-10',
+        'text-[50px] font-[600] tracking-wide text-center text-primary-contrast'
+      )}>
+        <span>spes</span>
+        <IconSearch className="size-12 fill-[#82a849]"/>
       </h1>
       <SearchBar />
       <main className="mt-10 flex flex-row gap-5">
@@ -24,7 +31,8 @@ const SearchPage = () => {
           <SearchResultsSummary />
           {/* Results */}
         </div>
-        <aside className="w-[300px]">
+        <aside className="flex flex-col gap-4 w-[400px]">
+          <GoToSearchEngine />
           <SidebarWikipedia />
         </aside>
       </main>
