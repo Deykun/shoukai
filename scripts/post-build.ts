94b1c14129472c51078fc2df98c58fab5d74821e
Szymon Tondowski <stdeykun+git@gmail.com>
2025-04-27 21:09:34 +0200

    🗃️ Subpage support added to GitHub release (should solve reindexing issue) (#22)
    * Release uncommented
    
    * Subpages support added to github release

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,13 +1,21 @@
-import fs from 'fs';
-import { fileURLToPath } from 'url'
-import { PATHS_DATA } from '../src/constants';
+import fs from "fs";
+import { PATHS_DATA } from "../src/constants";
 
-PATHS_DATA.forEach(({ path, title, social, lang = 'en' }) => {
-  let html = fs.readFileSync('./dist/index.html', 'utf-8');
+PATHS_DATA.forEach(({ path, title, social, lang = "en" }) => {
+  let html = fs.readFileSync("./dist/index.html", "utf-8");
 
-  html = html.replace('lang="en"', `lang="${lang}"`);
-  html = html.replace('<title>shoukai - personalized search</title>', `<title>${title}</title>`);
-  html = html.replace('<!-- SOCIAL -->', social);
+  if (lang) {
+    html = html.replace('lang="en"', `lang="${lang}"`);
+  }
+  if (title) {
+    html = html.replace(
+      "<title>shoukai - personalized search</title>",
+      `<title>${title}</title>`
+    );
+  }
+  if (social) {
+    html = html.replace("<!-- SOCIAL -->", social);
+  }
 
   fs.writeFileSync(`./dist/${path}.html`, html);
 });
