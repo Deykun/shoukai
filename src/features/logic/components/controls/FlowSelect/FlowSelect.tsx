a2124a374f77089ee30512a2213e9b9c7e39b765
Szymon Tondowski <stdeykun+git@gmail.com>
2026-08-16 00:31:13 +0200

    Basic options picker

--- src/features/logic/components/controls/FlowSelect/FlowSelect.tsx
+++ src/features/logic/components/controls/FlowSelect/FlowSelect.tsx
@@ -1,6 +1,7 @@
 import ButtonText from "@/components/UI/ButtonText";
 import { updateNode } from "@/features/logic/stores/useDiagramStore";
-import { memo } from "react";
+import { cn } from "@/utils/tailwind";
+import { memo, useState } from "react";
 import z from "zod";
 
 type Props = {
@@ -12,6 +13,7 @@ type Props = {
 };
 
 export const FlowSelect = memo(({ nodeId, value, schema }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
   const valueAsArray = Array.isArray(value) ? value : [value];
 
   const handleClick = () => {
@@ -23,8 +25,39 @@ export const FlowSelect = memo(({ nodeId, value, schema }: Props) => {
   console.log("options", schema?.shape?.compare?.options);
 
   return (
-    <ButtonText onClick={handleClick} size="small">
-      <span>{valueAsArray.join(" / ")}</span>
-    </ButtonText>
+    <div className="relative">
+      <ButtonText onClick={() => setIsOpen(!isOpen)} size="small">
+        <span>{valueAsArray.join(" / ")}</span>
+      </ButtonText>
+      {isOpen && (
+        <div
+          className={cn(
+            "absolute top-full left-1/2 -translate-x-1/2 z-[100]",
+            "p-3",
+            "bg-body",
+            "rounded-[18px] shadow-sm",
+            "border-[#f5f9ef] border",
+            "mx-auto font-[500] text-[16px]",
+            "hover:border-[#f5f9ef] hover:shadow-md",
+            "duration-500",
+          )}
+        >
+          {(schema?.shape?.compare?.options || []).map((option) => (
+            <ButtonText
+              size="small"
+              isActive={option === value}
+              onClick={() => {
+                updateNode(nodeId, {
+                  compare: option,
+                });
+                setIsOpen(false);
+              }}
+            >
+              <span>{option}</span>
+            </ButtonText>
+          ))}
+        </div>
+      )}
+    </div>
   );
 });
